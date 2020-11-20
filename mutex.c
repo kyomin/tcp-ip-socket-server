42f7f033575f0a685bda35ab15191d961bee1ce6
kyomin <kim031504@naver.com>
2020-11-20 23:36:34 +0900

    뮤텍스를 이용한 쓰레드 동기화

---
+++ mutex.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define NUM_THREAD	100
+
+void * thread_inc(void * arg);
+void * thread_des(void * arg);
+
+long long num = 0;
+
+/*
+	뮤텍스의 참조 값 저장을 위한 변수가 선언되었다.
+	이렇게 전역변수로 선언된 이유는 뮤텍스의 접근이 thread_inc, thread_des 이렇게 두 개의 함수 내에서 이뤄지기 때문이다.
+*/
+pthread_mutex_t mutex;
+
+int main(int argc, char *argv[]) {
+	pthread_t thread_id[NUM_THREAD];
+	int i;
+
+	pthread_mutex_init(&mutex, NULL);
+
+	for (i = 0; i < NUM_THREAD; i++) {
+		if (i % 2)
+			pthread_create(&(thread_id[i]), NULL, thread_inc, NULL);
+		else
+			pthread_create(&(thread_id[i]), NULL, thread_des, NULL);
+	}
+
+	for (i = 0; i < NUM_THREAD; i++)
+		pthread_join(thread_id[i], NULL);
+
+	printf("result: %lld \n", num);
+
+	/*
+		뮤텍스의 소멸을 보이고 있다.
+		이렇듯 뮤텍스는 필요가 없어지면 소멸해야 한다.
+	*/
+	pthread_mutex_destroy(&mutex);
+
+	return 0;
+}
+
+void * thread_inc(void * arg) {
+	int i;
+
+	/*
+		실제 임계영역은 num+=1 문장 하나이다.
+		그런데 여기서는 반복문까지 임계영역으로 포함해서 lock, unlock 함수를 호출하고 있다.
+		이와 관련해서는 README.md에 정리하였다.
+	*/
+	pthread_mutex_lock(&mutex);
+	for (i = 0; i < 50000000; i++)
+		num += 1;
+	pthread_mutex_unlock(&mutex);
+	
+	return NULL;
+}
+
+void * thread_des(void * arg) {
+	int i;
+	
+	for (i = 0; i < 50000000; i++) {
+		/*
+			임계영역에 해당하는 num-=1 문장만 뮤텍스의 lock, unlock 함수로 감싸고 있다.
+		*/
+		pthread_mutex_lock(&mutex);
+		num -= 1;
+		pthread_mutex_unlock(&mutex);
+	}
+
+	return NULL;
+}
